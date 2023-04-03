f04fe99ac46954f48687f43c9a36c3bf56f517bd
Kristina Alabastro <kristinaa7@live.com>
2023-04-02 23:25:51 -0700

    configure workbox plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,20 +11,44 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      // Webpack plugin that generates our html file and injects our bundles. 
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+    // Injects our custom service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
 
-    module: {
-      rules: [
-        
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'JATE',
+      description: 'Text coder to write and build your code',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
       ],
-    },
-  };
-};
+    }),
+  ],
